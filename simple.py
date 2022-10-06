776b24e977977e9130bba29d74c90f5cc6a23a2e
Phawer <69140238+Phawer-7@users.noreply.github.com>
2022-10-06 23:27:26 +0500

    malik

--- simple.py
+++ simple.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils.markdown import text, code
 from localization import *
 
-from config import bot_token
+from config import bot_token, chat_report
 
 bot = Bot(token=bot_token)
 dp = Dispatcher(bot)
@@ -16,11 +16,17 @@ async def get_id(message: types.Message):
         await message.answer(message.reply_to_message.from_user.id)
 
 
+@dp.message_handler(commands=['get_my_id'])
+async def get_id(message: types.Message):
+    if not message.reply_to_message:
+        await message.answer(message.from_user.id)
+    else:
+        await message.answer(message.reply_to_message.from_user.id)
+
+
 @dp.message_handler(commands=['start'])
 async def greeting(message: types.Message):
-    await message.answer(f"/гир - Получить самый обычный ник \n{code(text('/гир {цвет}'))} - Получить ник с "
-                         f"определенным кругом. {code(text('/гир белый'))} даст ник с белым кругом. \nДоступные цвета: "
-                         f"черный, желтый и белый")
+    await message.answer(f"Привет. /gir")
 
 
 @dp.message_handler(commands=['dev'])
@@ -35,6 +41,24 @@ async def get_id(message: types.Message):
 
 @dp.message_handler(commands=['Гор', "гор", "gor", "Gor"], commands_prefix="!/")
 async def send_nick_without_smiles(message: types.Message):
-    result = [i for i in message.from_user.first_name if i in normal_char or i in caps_normal_char]
+    if not message.reply_to_message:
+        userName = message.from_user.first_name
+        userId = message.from_user.id
+    else:
+        userName = message.reply_to_message.from_user.first_name
+        userId = message.reply_to_message.from_user.id
+
+    result = [i for i in userName if i in normal_char or i in caps_normal_char]
     ready_nick = "".join(result).lstrip()
-    await message.answer(ready_nick)
+    if userId == 819411604:
+        await message.answer(emoji.emojize(f':joystick:{ready_nick}:musical_note:'))
+    else:
+        await message.answer(ready_nick)
+
+    if not message.chat.title is None:
+        await bot.send_message(chat_report, f'{message.from_user.first_name} использовал {message.text} в '
+                                            f'{message.chat.title}(#{message.chat.id})')
+    else:
+        await bot.send_message(chat_report, f'{message.from_user.first_name} использовал {message.text} в '
+                                            f'в приватном чате(#{message.chat.id})')
+
