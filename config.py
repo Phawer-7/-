cae3124af7ab869464a00e6978b10b5f9719e83a
Phawer <69140238+Phawer-7@users.noreply.github.com>
2022-10-25 18:03:48 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-bot_token = "5704161549:AAFDzsQ_2a2zdxkaL7uFpYUMuhxkhSxT5s8"
-mongo = "mongodb+srv://phawer:GSlMIg8KULKH5UKK@cluster0.kmmemly.mongodb.net/?retryWrites=true&w=majority"
-
-# 5785425617:AAFGcJa0R1ARjV8DHlVCl0uNXNBAVjocSzs sav
-# 5704161549:AAFDzsQ_2a2zdxkaL7uFpYUMuhxkhSxT5s8 test
+bot_token = ""
+mongo = ""
 
 chat_report = -665464118
 command_chat = -1001674111955
