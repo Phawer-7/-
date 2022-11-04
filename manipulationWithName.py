7d1e894c0f415dc1522fec77a77d863a7d9c88fb
Phawer <69140238+Phawer-7@users.noreply.github.com>
2022-11-04 23:02:37 +0500

    Add files via upload

--- manipulationWithName.py
+++ manipulationWithName.py
@@ -1,4 +1,5 @@
 from mongoDB import getDefaultTriggerList
+from mongoDB import getDefaultTriggerList2
 
 
 def triggerList(getDefault: list) -> dict:
@@ -10,21 +11,33 @@ def triggerList(getDefault: list) -> dict:
 
 
 def returnWithoutSmiles(name: str) -> str:
-    dict2 = triggerList(getDefaultTriggerList())
-
+    dict2 = triggerList(getDefaultTriggerList2())
     new = name
-    result = list(new)
+
+    result = [i for i in list(new) if not i == ' ']
+    q = list(new)
     for i in dict2.values():
         for j in i:
-            try:
-                if new.startswith(j):
-                    del result[0:len(j)]
-                elif new.endswith(j):
-                    del result[result.index(j)]
-            except ValueError:
-                return 0
+            if new.startswith(j):
+                y = [i for i in q[:len(j)] if not i == ' ']
+                del result[:len(y)]
+            elif new.endswith(j):
+                try:
+                    ind = "".join(result).index(j)
+                    if ind+1 == len(result):
+                        del result[ind]
+                    else:
+                        del result[ind:]
+                except ValueError:
+                    pass
 
     res = "".join(result)
+    if res[:3] in new:
+        o = new.find(res[:3])
+        p = new.find(res[-3:])
+
+        res = new[o:p+3]
+
     if len(name) == len(res):
         return 0
     else:
